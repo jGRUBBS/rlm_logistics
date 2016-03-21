31b9602435cc215c38c9a01a961e99a8b668b3e4
Justin Grubbs <justin@jgrubbs.net>
2016-03-21 19:15:07 -0400

    extend read_timeout to 120 seconds since full inventory requests were timing out with the default of 60

--- lib/rlm_logistics/client.rb
+++ lib/rlm_logistics/client.rb
@@ -3,6 +3,10 @@ module RlmLogistics
 
     content_type "text/xml; charset=utf-8"
     host "www.ronlynn.com"
+    # RLM's API is particularly slow due to a large amount of data that is
+    # returned, so we bump the default Net::HTTP read_timeout to 120 seconds
+    # opposed to the default of 60 seconds
+    read_timeout 120
 
     def endpoint_path
       "#{RlmLogistics.endpoint_path}/services"
