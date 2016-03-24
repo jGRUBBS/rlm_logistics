0075f8b35f4cf2461b12f85b0a7b160abe798c70
Justin Grubbs <justin@jgrubbs.net>
2016-03-24 12:43:03 -0400

    add codeclimate test reporter - [ci skip]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rlm_logistics'
 require 'support/vcr_support'
